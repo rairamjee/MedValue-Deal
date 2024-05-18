bd2dc0ebd44023db3e27822636a936761728602d
Dipak-Natureglobal <dipak.mourya@natureglobal.comm>
2024-05-18 17:35:56 +0530

    dipak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Medicine from './Components/Medicine/Medicine';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
+    
     <div className="App">
       <AppNavbar/>
       <Medicine/>
